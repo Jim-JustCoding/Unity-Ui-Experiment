ec8144dfeab4286a8f7d17d55ec4dcd16dab4f7a
Jimmy Ouk <jimmy.ouk@students.mq.edu.au>
2022-10-21 10:59:57 +1100

    Fixed bug panel & button move panel infinitely.

--- UI Test/Assets/UIManager.cs
+++ UI Test/Assets/UIManager.cs
@@ -1,37 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DentedPixel;
 
 public class UIManager : MonoBehaviour
 {
-    public RectTransform window;
+    public RectTransform window; //Set Panel here
     public AnimationCurve animationCurve;
     public AnimationCurve animationCurve2;
-    private Vector3 startLocation;
+    private RectTransform startLocation;
     private Vector3 endLocation;
     public float time = 2f;
+    private Button panelButton;
 
     void Start()
     {
-        startLocation = window.position;
+        panelButton = window.GetComponent<Button>();
+        startLocation = window; // Saves panel's location on game start - set its position first in Scene
         Debug.Log(startLocation);
         Show();
     }
 
+    //Closing the Panel
     public void GoBack()
     {
-        LeanTween.moveX(window, transform.position.x - 130f, time).setEase(animationCurve2);
+        LeanTween.moveX(window, startLocation.position.x - 120, time).setEase(animationCurve2);
+        panelButton.enabled = true;
     }
 
+    //Opening the Panel
     public void Show()
     {
-        LeanTween.moveX(window, transform.position.x, time).setEase(animationCurve).setOnComplete(setLocation);
-    }
-
-    private void showMsg()
-    {
-        Debug.Log("Hello");
+        LeanTween.moveX(window, window.position.x + 120, time).setEase(animationCurve).setOnComplete(setLocation);
+        panelButton.enabled = false; //Prevents you from clicking on panel to move panel forward infinitely
     }
 
     private void setLocation()
