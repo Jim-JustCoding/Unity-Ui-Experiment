1ba3fe51d7e51616c3f30e42c452d37cab76be02
Jimmy Ouk <jimmy.ouk@students.mq.edu.au>
2022-10-21 10:27:58 +1100

    Added a side panel that slides in on start
    Using LeanTween, this panel slides in with a set Animation Curve, and a button lets it go back to its original position.

---
+++ UI Test/Assets/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform window;
+    public AnimationCurve animationCurve;
+    public AnimationCurve animationCurve2;
+    private Vector3 startLocation;
+    private Vector3 endLocation;
+    public float time = 2f;
+
+    void Start()
+    {
+        startLocation = window.position;
+        Debug.Log(startLocation);
+        Show();
+    }
+
+    public void GoBack()
+    {
+        LeanTween.moveX(window, transform.position.x - 130f, time).setEase(animationCurve2).setOnComplete(Show);
+    }
+
+    public void Show()
+    {
+        LeanTween.moveX(window, transform.position.x, time).setEase(animationCurve).setOnComplete(setLocation);
+    }
+
+    private void showMsg()
+    {
+        Debug.Log("Hello");
+    }
+
+    private void setLocation()
+    {
+        endLocation = window.position;
+        Debug.Log("EndLocation Set: " + endLocation);
+    }
+}
